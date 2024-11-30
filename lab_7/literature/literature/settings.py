fea4fcef2feebeed510c74165609d75d3153dc44
korableva-daniela <113258230+korableva-daniela@users.noreply.github.com>
2024-11-30 21:51:44 +0300

    Update settings.py

--- lab_7/literature/literature/settings.py
+++ lab_7/literature/literature/settings.py
@@ -75,13 +75,18 @@ WSGI_APPLICATION = "literature.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql',
-                           'NAME': 'prob',
-                           'USER': 'postgres',
-                           'PASSWORD': 'postpass',
-                           'HOST': 'localhost',
-                           'PORT': '5432', } }
-
+#DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql',
+#                           'NAME': 'prob',
+#                           'USER': 'postgres',
+#                           'PASSWORD': 'postpass',
+#                           'HOST': 'localhost',
+#                           'PORT': '5432', } }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
